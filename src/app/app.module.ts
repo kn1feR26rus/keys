8fc5c8f6ae1a3fa066f994b34f72e5f0228b80ec
Oleg Kovalenko <kn1feR26rus@yandex.ru>
2021-07-02 11:11:03 +0300

    header, intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { IntroComponent } from './components/main/intro/intro.component';
+import { KeysComponent } from './components/main/keys/keys.component';
+import { AboutComponent } from './components/main/about/about.component';
+import { ContactsComponent } from './components/main/contacts/contacts.component';
+import { AmenitiesComponent } from './components/main/amenities/amenities.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    IntroComponent,
+    KeysComponent,
+    AboutComponent,
+    ContactsComponent,
+    AmenitiesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/main/keys/keys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keys',
+  templateUrl: './keys.component.html',
+  styleUrls: ['./keys.component.scss']
+})
+export class KeysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
